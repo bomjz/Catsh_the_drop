1bc0ab926f02d539db40470ec3373b2f75a80402
Boris <b_khramtsov@mail.ru>
2022-08-28 13:37:14 +0300

    Add mouse listener and score

--- src/catch_the_drop/GameWindow.java
+++ src/catch_the_drop/GameWindow.java
@@ -3,6 +3,8 @@ package catch_the_drop;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 
 public class GameWindow extends JFrame {
@@ -15,6 +17,7 @@ public class GameWindow extends JFrame {
     private static float drop_left = 500;
     private static float drop_top = -100;
     private static float drop_v = 100;//Скорость капли
+    private static int score;
 
 
     public static void main(String[] args) throws IOException {
@@ -30,6 +33,27 @@ public class GameWindow extends JFrame {
         game_window.setResizable(false);//Запрет на измение размера окна
         last_frame_time = System.nanoTime(); //Возврат текущего времени в наносекундах
         GameField game_field = new GameField();
+        game_field.addMouseListener(new MouseAdapter() {
+            @Override // Когда кликаем на каплю,то:
+            public void mousePressed(MouseEvent e) {
+                //Координаты клика
+                int x = e.getX();
+                int y = e.getY();
+                //Координаты капли
+                float drop_right = drop_left + drop.getWidth(null);
+                float drop_bottom = drop_top + drop.getHeight(null);
+                //Проверка на попадание
+                boolean is_drop = x >= drop_left && x <= drop_right && y >= drop_top && y <= drop_bottom;
+                if (is_drop) {
+                    drop_top = -100;
+                    drop_left = (int) (Math.random() * (game_field.getWidth() - drop.getWidth(null)));
+                    drop_v += 20;
+                    score++;
+                    game_window.setTitle("Текущий счёт: " + score);
+                }
+
+            }
+        });
         game_window.add(game_field);
         game_window.setVisible(true);//Теперь окно видимо
 
@@ -43,7 +67,8 @@ public class GameWindow extends JFrame {
         drop_top = drop_top + drop_v * delta_time;
         g.drawImage(background1, 0, 0, null);
         g.drawImage(drop, (int) drop_left, (int) drop_top, null);
-//        g.drawImage(game_over,280,120,null);
+        if (drop_top > game_window.getHeight())
+            g.drawImage(game_over, 280, 120, null); //Вывод конца игры при пропуске капли
     }
 
     private static class GameField extends JPanel {
